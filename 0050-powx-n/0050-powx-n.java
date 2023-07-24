eadf6669fa50a168a74fb66ee23d2195868bfa27
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-07-24 13:54:41 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (84.40%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        return (double)Math.pow(x, n);
+        double temp;
+        if (n == 0) return 1;
+        temp = myPow(x, n / 2);
+        if (n % 2 == 0)
+            return temp * temp;
+        else {
+            if (n > 0)
+                return x * temp * temp;
+            else
+                return (temp * temp) / x;
+        }
     }
 }
