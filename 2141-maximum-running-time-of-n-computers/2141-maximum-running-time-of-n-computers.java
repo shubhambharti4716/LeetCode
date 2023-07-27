8ac20183ec9cfd08ccde41dea6a05df0e812902d
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-07-27 19:39:16 +0530

    Time: 12 ms (100.00%), Space: 57.5 MB (19.09%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -1,16 +1,23 @@
-public class Solution {
+class Solution {
     public long maxRunTime(int n, int[] batteries) {
-        Arrays.sort(batteries);
-        long left = 1, right = (long)Arrays.stream(batteries).asLongStream().sum() / n;
-        while (left < right) {
-            long target = right - (right - left) / 2;
-            long total = Arrays.stream(batteries).asLongStream().map(battery -> Math.min(battery, target)).sum();
-            if (total >= target * n) {
-                left = target;
-            } else {
-                right = target - 1;
-            }
+        int bn = batteries.length;
+        if(bn<n) return 0;
+        long l=0, r=0;
+        for(int b: batteries) r+=b;
+        r/=n;
+        long res=0;
+        while(l<=r) {
+            long mid = l+(r-l)/2;
+            if(possible(n, batteries, mid)) {
+                res=mid;
+                l=mid+1;
+            }else r=mid-1;
         }
-        return left;
+        return res;
+    }
+    private boolean possible(int n, int[] batteries, long target) {
+        long totalTarget = target*n;
+        for(int b: batteries) totalTarget-=Math.min(target, b);
+        return totalTarget<=0;
     }
 }
