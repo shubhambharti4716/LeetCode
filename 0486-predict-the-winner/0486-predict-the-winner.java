806473ee125dad0c6e87f78761350f47b8f8017a
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-07-28 16:49:40 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (30.09%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n]; 
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = nums[i]; // base case- the player can pick only one number, so                                    their score is the number itself
-
-            for (int j = i + 1; j < n; j++) {
-                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+        int[] dp = Arrays.copyOf(nums, n);
+        
+        for (int difference = 1; difference < n; difference++) {
+            for (int left = 0; left < n - difference; left++) {
+                int right = left + difference;
+                dp[left] = Math.max(nums[left] - dp[left + 1], nums[right] - dp[left]);
             }
         }
-
-        // The first player's score is stored in dp[n-1].
-        // If the score is greater than or equal to 0, the first player wins;                       otherwise, the second player wins.
-        return dp[n - 1] >= 0;
+        
+        return dp[0] >= 0;
     }
-}
+}   
