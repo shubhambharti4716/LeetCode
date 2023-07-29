c9ddc57912da1e24b429056882426baac998ad3c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-07-29 18:59:52 +0530

    Added solution - LeetHub

--- Median of BST - GFG/median-of-bst.java
+++ Median of BST - GFG/median-of-bst.java
@@ -112,61 +112,56 @@ class GfG {
 
 //User function Template for Java
 
-class Tree
-{
-   public static int countNodes(Node n)
-{
-    // this function returns number of nodes in tree
-    if(n==null) return 0;
-    return 1 + countNodes(n.left) + countNodes(n.right);
-}
-
-public static int find(Node n, int serialNo, int target, int value[])
-{
-    // this function keeps track of serial number while doing
-    // inorder traveresal and finds the target node
-    
-    if(n==null) return serialNo;
-    serialNo = find(n.left, serialNo, target, value);
-    
-    serialNo++;
-    // current value of serialNo variable is the rank of
-    // current Node
-    if(serialNo==target) value[0] = n.data;
-    
-    serialNo = find(n.right, serialNo, target, value);
-    return serialNo;
-}
-
-public static float findMedian(Node root)
-{
-    int n = countNodes(root);
-    
-    if(n%2!=0)
-    {
-        // odd number of nodes in tree:
-        // 1+n/2 is the rank of mid node
-        
-        int mid[] = new int[1];
-        mid[0] = 0;
-        find( root, 0, 1+n/2, mid );
-        return (float)(mid[0]);
+class Tree {
+   public static int countNodes(Node n) {
+        // this function returns number of nodes in tree
+        if(n==null) return 0;
+        return 1 + countNodes(n.left) + countNodes(n.right);
     }
-    else
-    {
-        // even number of nodes in tree:
-        // n/2 and 1+n/2 are the ranks of mid nodes
-        
-        int mid1[] = new int[1];
-        int mid2[] = new int[1];
-        mid1[0] = 0;
-        mid2[0] = 0;
-        find( root, 0, n/2, mid1 );
-        find( root, 0, 1+n/2, mid2 );
-        if( (mid1[0]+mid2[0])%2 == 0)
-            return (float)(mid1[0]+mid2[0])/2;
-        else    
-            return ((float)(mid1[0]+mid2[0])/2);
+
+    public static int find(Node n, int serialNo, int target, int value[]) {
+        // this function keeps track of serial number while doing
+        // inorder traveresal and finds the target node
+    
+        if(n==null) return serialNo;
+        serialNo = find(n.left, serialNo, target, value);
+    
+        serialNo++;
+        // current value of serialNo variable is the rank of
+        // current Node
+        if(serialNo==target) value[0] = n.data;
+    
+        serialNo = find(n.right, serialNo, target, value);
+        return serialNo;
     }
-}
+
+    public static float findMedian(Node root) {
+        int n = countNodes(root);
+    
+        if(n%2!=0) {
+            // odd number of nodes in tree:
+            // 1+n/2 is the rank of mid node
+        
+            int mid[] = new int[1];
+            mid[0] = 0;
+            find( root, 0, 1+n/2, mid );
+            return (float)(mid[0]);
+        }
+        else
+        {
+            // even number of nodes in tree:
+            // n/2 and 1+n/2 are the ranks of mid nodes
+        
+            int mid1[] = new int[1];
+            int mid2[] = new int[1];
+            mid1[0] = 0;
+            mid2[0] = 0;
+            find( root, 0, n/2, mid1 );
+            find( root, 0, 1+n/2, mid2 );
+            if( (mid1[0]+mid2[0])%2 == 0)
+                return (float)(mid1[0]+mid2[0])/2;
+            else    
+                return ((float)(mid1[0]+mid2[0])/2);
+        }
+    }
 }
