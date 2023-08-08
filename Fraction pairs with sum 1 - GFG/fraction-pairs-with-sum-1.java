84e1fc79b6575048648158e117860c365c5900cd
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-08-08 17:20:27 +0530

    Added solution - LeetHub

--- Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.java
+++ Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.java
@@ -60,33 +60,49 @@ class GFG {
 
 
 class Solution {
-    public static int countFractions(int n, int[] numerator, int[] denominator) {
-        // code here
-        int ans=0;
-        HashMap<Integer,HashMap<Integer,Integer>> hm=new HashMap<>();
-        for(int i=0;i<n;i++){
-            int x=numerator[i],y=denominator[i];
-            int g=gcd(x,y);
-            x/=g;
-            y/=g;
-            if(hm.containsKey(y-x) && hm.get(y-x).containsKey(y)){
-                ans+=hm.get(y-x).get(y);
-            }
-            if(hm.containsKey(x)){
-                HashMap<Integer,Integer> internalMap=hm.get(x);
-                internalMap.put(y,internalMap.getOrDefault(y,0)+1);
-            }else{
-                HashMap<Integer,Integer> internalMap=new HashMap<>();
-                internalMap.put(y,1);
-                hm.put(x,internalMap);
-            }
-        }
-        return ans;
-    }
     public static int gcd(int a,int b)
     {
-        if(b==0)return a;
-        return gcd(b,a%b);
+        if(a==0) return b;
+        if(b==0) return a;
+        if(a==b) return a;
+        if(a>b) return gcd(a-b,b);
+        return gcd(a,b-a);
+    }
+    public static int countFractions(int n, int[] numerator, int[] denominator) {
+        // code here
+        int fractions=0;
+        for(int i=0;i<n;i++)
+        {
+            int gcd=gcd(numerator[i],denominator[i]);
+            numerator[i]/=gcd;
+            denominator[i]/=gcd;
+        }
+        Hashtable<List<Integer>,Integer> hash=new Hashtable<>();
+        for(int i=0;i<n;i++)
+        {
+            int x=numerator[i];
+            int y=denominator[i];
+            int z=y-x;
+            List<Integer> lst=new ArrayList<>();
+            lst.add(z);
+            lst.add(y);
+            if(hash.get(lst)!=null)
+            {
+                fractions+=hash.get(lst);
+            }
+            List<Integer> lst2=new ArrayList<>();
+            lst2.add(x);
+            lst2.add(y);
+            if(hash.get(lst2)!=null)
+            {
+                hash.replace(lst2,hash.get(lst2)+1);
+            }
+            else
+            {
+                hash.put(lst2,1);
+            }
+        }
+        return fractions;
     }
 }
         
