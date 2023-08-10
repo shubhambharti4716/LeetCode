09f32c99753fda0c8373a96dcb0b954761551bde
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-08-10 19:30:05 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (96.66%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        List<Integer>list=new LinkedList<>();
-        for(int num:nums){
-            list.add(num);
+        
+        int n = nums.length;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                return true;
+            }
         }
-        return list.contains(target);
+        return false;
     }
 }
