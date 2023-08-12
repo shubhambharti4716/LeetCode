0debe63493b926abd7215117cea501f4ef276434
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-08-12 21:37:44 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (6.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,50 +1,24 @@
-class Solution {
-    public int uniquePathsWithObstacles(int[][] mat) {
-        // return tabulation(mat);
-        return top_down(mat);
-    }
-
-    private int top_down(int[][] mat){
-        int n = mat.length;
-        int m = mat[0].length;
-
-        int[][] dp = new int[n][m];
-
-        return func(0,0,dp,mat);
-    }
-
-    private int func(int n,int m,int[][] dp,int[][] mat){
-        if(n >= dp.length || m >= dp[0].length || mat[n][m] == 1)return 0;
-        if(n == dp.length-1 && m == dp[0].length-1)return 1;
-        
-        if(dp[n][m] != 0)return dp[n][m];
-
-        return dp[n][m] = func(n,m+1,dp,mat) + func(n+1,m,dp,mat);
-    }
-
-    private int tabulation(int[][] mat){
-        int n = mat.length;
-        int m = mat[0].length;
-
-        int[][] dp = new int[n][m];
-
-        for(int i=0;i<n;i++){
-            if(mat[i][0] == 1)break;
-            dp[i][0] = 1;
-        }
-        
-        for(int i=0;i<m;i++){
-            if(mat[0][i] == 1)break;
-            dp[0][i] = 1;
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
+            return 0;
         }
 
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(mat[i][j] == 1)continue;
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[] previous = new int[n];
+        int[] current = new int[n];
+        previous[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
+            for (int j = 1; j < n; j++) {
+                current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j-1] + previous[j];
             }
+            System.arraycopy(current, 0, previous, 0, n);
         }
 
-        return dp[n-1][m-1];
+        return previous[n-1];
     }
 }
