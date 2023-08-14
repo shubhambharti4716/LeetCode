fc119c0a6e9bf658164b549ff1649cc1b473575c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-08-14 20:04:27 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        // Pass 1 : 
+        // Get the XOR of the two numbers we need to find
+        int diff = 0;
+        for(int i: nums)
+        	diff = i ^ diff;
+        // Get its last set bit
+        diff &= -diff;
+        // Pass 2 :
+        int rets[] = {0, 0}; // this vector stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        if(rets[0] > rets[1])
+        {
+            int temp=rets[0];
+            rets[0]=rets[1];
+            rets[1]=temp;
+        }
+        return rets;
+    }
+}
