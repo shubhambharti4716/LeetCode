19b8223b027e398ac719a01d1487cc9dac230c0c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-08-19 20:29:15 +0530

    Time: 10 ms (100.00%), Space: 44.5 MB (54.55%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
@@ -1,97 +1,117 @@
 class Solution {
-    class UnionFind{
-        int[] root;
-        int[] size;
-        UnionFind(int n){
-            root = new int[n];
-            for(int i=0;i<n;i++)
-                root[i] = i;
-            size = new int[n];
-            Arrays.fill(size, 1);
-        }
-
-        int find(int x){
-            while(root[x] != x){
-                x = root[root[x]];
-            }
-            return x;
-        }
-
-        boolean merge(int x, int y){
-            int rx = find(x);
-            int ry = find(y);
-            if(rx == ry)
-                return false;
-            if(size[rx]>size[ry]){
-                root[ry] = rx;
-                size[rx]+=size[ry];
-            }else{
-                root[rx]=ry;
-                size[ry] += size[rx];
-            }
-            return true;
-        }
-    }
     public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
-        List<List<Integer> > ans = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>(2);
         ans.add(new ArrayList<>());
         ans.add(new ArrayList<>());
-
-        for(int i=0;i<edges.length;i++){
-            edges[i] = new int[]{edges[i][0], edges[i][1], edges[i][2], i};
+        int m = edges.length;
+        int[][] es = new int[m][4];
+        for (int i = 0; i < m; i++) {
+            es[i][0] = edges[i][0];
+            es[i][1] = edges[i][1];
+            es[i][2] = edges[i][2];
+            es[i][3] = i;
         }
-
-        Arrays.sort(edges, (a,b)->a[2]-b[2]);
-
-        int min = minCost(edges, n, -1, -1);
-        // System.out.println("min="+min);
-
-        // for(int[] e:edges)
-            // System.out.println(Arrays.toString(e));
-
-        for(int i=0;i<edges.length;i++){
-            int m1 = minCost(edges, n, i, -1);
-            // System.out.printf("exclude:%s, %d", Arrays.toString(edges[i]), m1);
-            if(m1>min){
-                ans.get(0).add(edges[i][3]);
-            }else{
-                int m2 = minCost(edges, n, -1, i);
-                // System.out.printf("include:%s, %d", Arrays.toString(edges[i]), m2);
-                if(m2==min){
-                    ans.get(1).add(edges[i][3]);
+        Arrays.sort(es, (a, b) -> a[2] - b[2]);
+        List<List<int[]>> buffer = new ArrayList<>();
+        List<int[]> cur = new ArrayList<>();
+        cur.add(es[0]);
+        for (int i = 1; i < m; ++i) {
+            int[] e = es[i];
+            if (cur.get(0)[2] < e[2]) {
+                buffer.add(cur);
+                cur = new ArrayList<>();
+            }
+            cur.add(e);
+        }
+        buffer.add(cur);
+        UF uf = new UF(n);
+        for (List<int[]> slot: buffer) {
+            int l = slot.size();
+            List<int[]> candidates = new ArrayList<>();
+            for (int i = 0; i < l; ++i) {
+                int[] e = slot.get(i);
+                if (uf.isConnected(e[0], e[1])) {
+                    continue;
+                }
+                candidates.add(e);
+            }
+            l = candidates.size();
+            for (int i = 0; i < l; ++i) {
+                UF cp = uf.clone();
+                int[] e0 = candidates.get(i);
+                for (int j = 0; j < l; ++j) {
+                    if (j == i) {
+                        continue;
+                    }
+                    int[] e = candidates.get(j);
+                    cp.union(e[0], e[1]);
+                }
+                if (cp.isConnected(e0[0], e0[1])) {
+                    ans.get(1).add(e0[3]);
+                } else {
+                    ans.get(0).add(e0[3]);
                 }
             }
+            for (int[] e: candidates) {
+                uf.union(e[0], e[1]);
+            }
         }
-
         return ans;
-
     }
 
-    int minCost(int[][] edges, int n, int exclude, int mustInclude){
-        UnionFind uf = new UnionFind(n);
-        int cnt = 0;
-        int sum = 0;
-        if(mustInclude>=0){
-            uf.merge(edges[mustInclude][0], edges[mustInclude][1]);
-            sum += edges[mustInclude][2];
-            cnt++;
+    private static final class UF {
+
+        private UF(int n) {
+            this.n = n;
+            this.size = n;
+            parent = new int[size];
+            sz = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                sz[i] = 1;
+            }
         }
 
-        for(int i=0;i<edges.length;i++){
-            if(i==exclude  || i == mustInclude)
-                continue;
-            int[] e = edges[i];
-            int u = e[0];
-            int v = e[1];
-            int c = e[2];
-            if(uf.merge(u, v)){
-                cnt++;
-                sum+=c;
-            }
-            if(cnt == n-1)
-                return sum;
+        private int[] parent;
+        private int[] sz;
+        private int size;
+        private int n;
+
+        @Override
+        public UF clone() {
+            UF copy = new UF(1);
+            copy.size = this.size;
+            copy.n = this.n;
+            copy.sz = this.sz.clone();
+            copy.parent = this.parent.clone();
+            return copy;
         }
-        // it is possible not compose a connect tree, so it is showing it is critical path if removed.
-        return cnt==n-1?sum:Integer.MAX_VALUE;
+
+        private int find(int x) {
+            if (x == parent[x]) {
+                return x;
+            }
+            return parent[x] = find(parent[x]);
+        }
+
+        private void union(int x, int y) {
+            int xRoot = find(x);
+            int yRoot = find(y);
+            if (xRoot == yRoot) {
+                return;
+            }
+            if (sz[xRoot] > sz[yRoot]) {
+                parent[yRoot] = xRoot;
+                sz[xRoot] += sz[yRoot];
+            } else {
+                parent[xRoot] = yRoot;
+                sz[yRoot] += sz[xRoot];
+            }
+            --size;
+        }
+
+        private boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
     }
 }
