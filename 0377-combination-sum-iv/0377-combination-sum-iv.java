d1bc73fb632ab5c4cda36f8d1c30e978a9ec6d60
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-09 08:48:48 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (73.34%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,25 +1,40 @@
-class Solution {
-    public int combinationSum4(int[] nums, int target) {
-        return findAll(target, nums, new HashMap<>());
-    }
-    
-     public int findAll(int target, int[] candidates, HashMap<Integer, Integer> map) {
-        if(target == 0) {
+public class Solution {
+    private static final int MAX_TARGET = 1010; // Maximum possible target value
+    private int[] dp = new int[MAX_TARGET]; // Array to store computed results
+
+    private int countCombinations(int[] nums, int remainingTarget) {
+        // If the remaining target is 0, there's one valid combination.
+        if (remainingTarget == 0)
             return 1;
+        
+        // If the remaining target becomes negative, it's not possible to reach it.
+        if (remainingTarget < 0)
+            return 0;
+        
+        // If the result for 'remainingTarget' is already computed, return it.
+        if (dp[remainingTarget] != -1)
+            return dp[remainingTarget];
+        
+        int currentCombinations = 0;
+        
+        // Iterate through the numbers in 'nums'.
+        for (int i = 0; i < nums.length; i++) {
+            int currentNum = nums[i];
+            // Recursively calculate combinations for the new target.
+            currentCombinations += countCombinations(nums, remainingTarget - currentNum);
         }
+        
+        // Store and return the computed result.
+        return dp[remainingTarget] = currentCombinations;
+    }
 
-        if(map.containsKey(target))
-            return map.get(target);
-
-        int sum = 0;
-        for(int i=0; i<candidates.length; i++) {
-            if(target < candidates[i])
-                continue;
-            sum += findAll(target-candidates[i], candidates, map);
+    public int combinationSum4(int[] nums, int target) {
+        // Initialize the 'dp' array with -1 to indicate uncomputed results.
+        for (int i = 0; i < MAX_TARGET; i++) {
+            dp[i] = -1;
         }
-
-        map.put(target, sum);
-
-        return sum;
+        
+        // Start the combination count calculation.
+        return countCombinations(nums, target);
     }
 }
