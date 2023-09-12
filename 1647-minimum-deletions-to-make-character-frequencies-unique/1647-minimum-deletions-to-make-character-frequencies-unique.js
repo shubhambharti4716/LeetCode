6411fecfb6db8e3b902e215e1fc3408e886d94d7
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-12 07:33:00 +0530

    Time: 69 ms (97.67%), Space: 46.5 MB (90.70%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function(s) {
+    let freq = new Array(26).fill(0); // Create an array to store character frequencies
+    
+    for (let i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 'a'.charCodeAt(0)]++; // Count the frequency of each character
+    }
+    
+    freq.sort((a, b) => a - b); // Sort frequencies in ascending order
+    
+    let del = 0; // Initialize the deletion count
+    
+    for (let i = 24; i >= 0; i--) {
+        if (freq[i] === 0) {
+            break; // No more characters with this frequency
+        }
+        
+        if (freq[i] >= freq[i + 1]) {
+            let prev = freq[i];
+            freq[i] = Math.max(0, freq[i + 1] - 1);
+            del += prev - freq[i]; // Update the deletion count
+        }
+    }
+    
+    return del; // Return the minimum deletions required
+};
