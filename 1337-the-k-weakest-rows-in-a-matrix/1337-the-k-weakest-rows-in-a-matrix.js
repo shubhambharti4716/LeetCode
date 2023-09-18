13cfdcb9d7757612e540f29adb701f1e45e8067c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-18 21:56:47 +0530

    Time: 62 ms (32.24%), Space: 43.6 MB (95.07%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -4,22 +4,7 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    let countedMat = mat.map(x => x.reduce( (a, b) => a + b));
-    
-    let indexObj = {};
-    
-    for(let i = 0; i < countedMat.length; i += 1){
-        let key = countedMat[i];
-        indexObj[countedMat.indexOf(key)] = key;
-        // overwrite matrix to allow indexOf to find duplicated
-        countedMat[i] = -1; 
-    }
-
-    let keyValues = Object.entries(indexObj);
-
-    let sortedValues = keyValues.sort( (a, b) => a[1] - b[1]);
-
-    let finalIdx = sortedValues.map(x => x[0]).slice(0, k)
-
-    return finalIdx;
+    let rowStrengths = mat.map((row, i) => [row.reduce((a, b) => a + b, 0), i]);
+    rowStrengths.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+    return rowStrengths.slice(0, k).map(([_, i]) => i);
 };
