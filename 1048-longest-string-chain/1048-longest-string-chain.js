03444da379db058fdb1341cc8f95871548a1b75f
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-23 11:53:40 +0530

    Time: 85 ms (89.09%), Space: 47.9 MB (76.36%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.js
+++ 1048-longest-string-chain/1048-longest-string-chain.js
@@ -4,17 +4,16 @@
  */
 var longestStrChain = function(words) {
     words.sort((a, b) => a.length - b.length);
-    const dp = {};
-    let max_chain = 0;
-    for (const word of words) {
-        dp[word] = 1;
-        for (let i = 0; i < word.length; i++) {
-            const prev_word = word.slice(0, i) + word.slice(i + 1);
-            if (prev_word in dp) {
-                dp[word] = Math.max(dp[word], dp[prev_word] + 1);
-            }
-        }
-        max_chain = Math.max(max_chain, dp[word]);
-    }
-    return max_chain;
+	const map = new Map();
+	let result = 0;
+	for (let word of words) {
+		let max = 0;
+		for (let i = 0; i < word.length; i++) {
+			const prev = word.slice(0, i) + word.slice(i + 1);
+			max = Math.max(max, (map.get(prev) || 0) + 1);
+		}
+		map.set(word, max);
+		result = Math.max(result, max);
+	}
+	return result;
 };
