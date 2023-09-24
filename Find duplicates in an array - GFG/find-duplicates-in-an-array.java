8ecca601405b0fa9f978b90c7ca2257ddebd09c5
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-24 20:18:56 +0530

    Added solution - LeetHub

---
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            Solution g = new Solution();
+            ArrayList<Integer> ans = g.duplicates(a, n);
+            for (Integer val : ans) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        // First check all the values that are
+        // present in an array then go to that
+        // values as indexes and increment by
+        // the size of array
+        for (int i = 0; i < n; i++) {
+            int index = arr[i] % n;
+            arr[index] += n;
+        }
+
+        // Now check which value exists more
+        // than once by dividing with the size
+        // of array
+        int flag = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] / n) > 1) {
+                ans.add(i);
+                flag = 1;
+            }
+        }
+        if (flag == 0) ans.add(-1);
+        return ans;
+    }
+}
