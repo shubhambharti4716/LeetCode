5335cc5830c01aefa1a0e598a894566e3c3d3f1a
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-29 18:59:04 +0530

    Time: 66 ms (95.24%), Space: 53.6 MB (22.04%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -3,11 +3,21 @@
  * @return {boolean}
  */
 var isMonotonic = function(nums) {
-    let incr = true;
-    let decr = true;
-    for(let i = 0; i < nums.length - 1; i++){
-        if (nums[i] > nums[i + 1]) incr = false;
-        if (nums[i] < nums[i + 1]) decr = false;
+
+    let increasing = true;
+    let decreasing = true;
+
+    for(let i = 1 ; i < nums.length ; i++){
+
+        if(nums[i] < nums[i - 1]){
+            increasing = false;
+        }
+
+        if(nums[i] > nums[i - 1]){
+            decreasing = false;
+        }
     }
-    return incr || decr
+
+    return increasing || decreasing;
+    
 };
