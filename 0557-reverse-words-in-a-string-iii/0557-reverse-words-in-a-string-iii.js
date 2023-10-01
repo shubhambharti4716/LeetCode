5fdda948c301c8e086ce53dea061c84957feb4f5
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-01 14:20:24 +0530

    Time: 66 ms (51.92%), Space: 48.2 MB (64.01%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s = s.split(' ')
+    for (i = 0; i < s.length; i++){
+        s[i] = s[i].split('').reverse().join('')
+    }
+    return s.join(' ')
+}
