f9d9fd9d19dd64ea84e1c8fee0ea4a07ba9e01a0
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-01 14:21:15 +0530

    Time: 69 ms (36.88%), Space: 49.8 MB (12.71%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -2,10 +2,21 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    s = s.split(' ')
-    for (i = 0; i < s.length; i++){
-        s[i] = s[i].split('').reverse().join('')
+var reverseWords = function(s){
+        let chars = [...s];
+        let start = 0;
+
+        for (let i = 0; i < chars.length; i++) {
+            if (chars[i] === ' ' || i === chars.length - 1) {
+                let end = (i === chars.length - 1 && chars[i] !== ' ') ? i + 1 : i;
+                while (start < end) {
+                    [chars[start], chars[end - 1]] = [chars[end - 1], chars[start]];
+                    start++;
+                    end--;
+                }
+                start = i + 1;
+            }
+        }
+
+        return chars.join('');
     }
-    return s.join(' ')
-}
