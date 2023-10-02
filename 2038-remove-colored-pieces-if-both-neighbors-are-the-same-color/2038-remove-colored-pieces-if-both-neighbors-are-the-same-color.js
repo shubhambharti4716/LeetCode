a4a0f43295f383a0bc7f6f03bff6c4570f31a21e
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-02 20:01:37 +0530

    Time: 66 ms (88.33%), Space: 45.7 MB (41.67%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function(colors) {
+        let alice_plays = 0, bob_plays = 0, count = 0;
+        
+        for (let i = 1; i < colors.length; i++) {
+            if (colors[i] == colors[i - 1]) {
+                count++;
+            } else {
+                if (colors[i - 1] == 'A') {
+                    alice_plays += Math.max(0, count - 1);
+                } else {
+                    bob_plays += Math.max(0, count - 1);
+                }
+                count = 0;
+            }
+        }
+
+        if (colors.charAt(colors.length - 1) == 'A') {
+            alice_plays += Math.max(0, count - 1);
+        } else {
+            bob_plays += Math.max(0, count - 1);
+        }
+        
+        return alice_plays > bob_plays;
+    }
