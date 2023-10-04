ff50b3a38936364c12946763dd8183911e1541b7
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-04 18:47:12 +0530

    Added solution - LeetHub

---
+++ Roman Number to Integer - GFG/roman-number-to-integer.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.romanToDecimal(roman));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int value(char r) {
+        if (r == 'I') return 1;
+        if (r == 'V') return 5;
+        if (r == 'X') return 10;
+        if (r == 'L') return 50;
+        if (r == 'C') return 100;
+        if (r == 'D') return 500;
+        if (r == 'M') return 1000;
+        return -1;
+    }
+
+    // Finds decimal value of a given romal numeral
+    public int romanToDecimal(String str) {
+        // Initialize result
+        int res = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            // Getting value of symbol s[i]
+            int s1 = value(str.charAt(i));
+
+            // Getting value of symbol s[i+1]
+            if (i + 1 < str.length()) {
+                int s2 = value(str.charAt(i + 1));
+
+                // Comparing both values
+                if (s1 >= s2) {
+                    // Value of current symbol is greater
+                    // or equalto the next symbol
+                    res = res + s1;
+                } else {
+                    res = res + s2 - s1;
+                    i++; // Value of current symbol is
+                    // less than the next symbol
+                }
+            } else {
+                res = res + s1;
+                
+            }
+        }
+
+        return res;
+    }
+}
