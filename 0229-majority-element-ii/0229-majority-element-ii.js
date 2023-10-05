c4a9481079cc5f2d944b7404b1fe13b9f0edf9a1
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-05 12:35:06 +0530

    Time: 59 ms (59.05%), Space: 44.7 MB (37.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,37 +3,20 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let candidate1 = null, candidate2 = null, count1 = 0, count2 = 0;
-    
-    for (let num of nums) {
-        if (num === candidate1) {
-            count1++;
-        } else if (num === candidate2) {
-            count2++;
-        } else if (count1 === 0) {
-            candidate1 = num;
-            count1 = 1;
-        } else if (count2 === 0) {
-            candidate2 = num;
-            count2 = 1;
-        } else {
-            count1--;
-            count2--;
-        }
+    const count = new Map();
+    const freq = []
+  for(let num of nums) {
+      let newCount = 1;
+      if(count.has(num)) {
+            newCount += count.get(num);
+          count.set(num, newCount);
+          
+      } else {
+          count.set(num, 1);
+      }
+      if(newCount > Math.floor((nums.length)/3) && !freq.includes(num)) {
+        freq.push(num);
     }
-    
-    count1 = count2 = 0;
-    
-    for (let num of nums) {
-        if (num === candidate1) count1++;
-        else if (num === candidate2) count2++;
-    }
-    
-    const threshold = Math.floor(nums.length / 3);
-    const result = [];
-    
-    if (count1 > threshold) result.push(candidate1);
-    if (count2 > threshold) result.push(candidate2);
-    
-    return result;
+  }  
+  return freq;
 };
