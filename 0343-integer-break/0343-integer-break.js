c2042fc1a8d23c86dd629440224da15e5915374d
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-06 19:29:22 +0530

    Time: 55 ms (36.00%), Space: 41.6 MB (89.00%) - LeetHub

--- 0343-integer-break/0343-integer-break.js
+++ 0343-integer-break/0343-integer-break.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var integerBreak = function(n) {
-    let x = 1;
-    while (n > 4) {
-        x *= 3;
-        n -= 3
-    } 
-    return x * (x > 1 ? n : n === 3 ? 2 : n === 4 ? 4 : 1);
-    
+    if (n === 2) return 1
+    if (n === 3) return 2
+    if (n % 3 == 0) return Math.pow(3, n / 3)
+    if (n % 3 == 1) return 4 * Math.pow(3, (n - 4) / 3)
+    return 2 * Math.pow(3, (n - 2) / 3)
 };
