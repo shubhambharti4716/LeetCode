e4a19250fa0da8e462ad754d9721021a4a12dce6
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-08 20:41:07 +0530

    Time: 67 ms (75.00%), Space: 45.9 MB (87.50%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.js
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.js
@@ -3,27 +3,31 @@
  * @param {number[]} nums2
  * @return {number}
  */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var maxDotProduct = function(nums1, nums2) {
-    const n = nums1.length;
-    const m = nums2.length;
-    const memo = new Array(n).fill(null).map(() => new Array(m).fill(Number.MIN_SAFE_INTEGER));
-    
-    const dp = (i, j) => {
-        if (i === n || j === m) {
-            return Number.MIN_SAFE_INTEGER;
+    const n = nums1.length, m = nums2.length;
+    const dp = Array.from({length: n}, () => new Int32Array(m));
+    dp[0][0] = nums1[0] * nums2[0];
+
+    for(let i = 1; i < n; ++i) {
+        dp[i][0] = Math.max(nums1[i] * nums2[0], dp[i - 1][0]);
+    }
+
+    for(let j = 1; j < m; ++j) {
+        dp[0][j] = Math.max(nums1[0] * nums2[j], dp[0][j - 1]);
+    }
+
+    for(let i = 1; i < n; ++i) {
+        for(let j = 1; j < m; ++j) {
+            dp[i][j] = nums1[i] * nums2[j] + Math.max(dp[i - 1][j - 1], 0);
+            dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+            dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
         }
-        
-        if (memo[i][j] !== Number.MIN_SAFE_INTEGER) {
-            return memo[i][j];
-        }
-        
-        memo[i][j] = Math.max(
-            nums1[i] * nums2[j] + Math.max(dp(i + 1, j + 1), 0),
-            Math.max(dp(i + 1, j), dp(i, j + 1))
-        );
-        
-        return memo[i][j];
-    };
-    
-    return dp(0, 0);
+    }
+
+    return dp[n - 1][m - 1];
 };
