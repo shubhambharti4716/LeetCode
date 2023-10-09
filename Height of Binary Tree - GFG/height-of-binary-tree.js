c5946f09a866b4241cf914b333ffa1d8d0e0663e
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-09 13:11:23 +0530

    Added solution - LeetHub

---
+++ Height of Binary Tree - GFG/height-of-binary-tree.js
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str){
+   // Corner Case
+   if(str.length === 0 || str[0] === "N")
+       return null;
+
+   // Create the root of the tree
+   let root = new Node(parseInt(str[0]));
+
+   // Push the root to the queue
+   let queue = [];
+   let start = 0;
+   queue.push(root);
+
+   // Starting from the second element
+   let i = 1;
+   while(queue.length !== start && i < str.length) {
+
+       // Get and remove the front of the queue
+       let currNode = queue[start];
+       start++;
+
+       // Get the current node's value from the string
+       let currVal = str[i];
+
+       // If the left child is not null
+       if(currVal !== "N") {
+
+           // Create the left child for the current node
+           currNode.left = new Node(parseInt(currVal));
+
+           // Push it to the queue
+           queue.push(currNode.left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= str.length)
+           break;
+       currVal = str[i];
+
+       // If the right child is not null
+       if(currVal !== "N") {
+
+           // Create the right child for the current node
+           currNode.right = new Node(parseInt(currVal));
+
+           // Push it to the queue
+           queue.push(currNode.right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar1 = readLine().split(' ');
+        let root = buildTree(input_ar1);
+        let obj = new Solution();
+        console.log(obj.height(root));
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {Node} node
+ * @returns {number}
+*/
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    //Function to find the height of a binary tree.
+    height(node)
+    {
+        //if node is null, we return 0.
+        if (node === null)
+            return 0;
+
+        //else we call the recursive function, height for left and right 
+        //subtree and choose their maximum. we also add 1 to the result
+        //which indicates height of root of the tree.
+        else
+            return 1+ Math.max(this.height(node.left), this.height(node.right));
+    }
+}
