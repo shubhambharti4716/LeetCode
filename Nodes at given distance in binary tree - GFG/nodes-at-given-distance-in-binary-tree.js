f73b4170d50efc9e98e6a1e153f0df59a9c615be
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-10 18:51:02 +0530

    Added solution - LeetHub

---
+++ Nodes at given distance in binary tree - GFG/nodes-at-given-distance-in-binary-tree.js
@@ -0,0 +1,197 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+class Node
+{
+    constructor(x){
+        this.key=x;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+function buildTree(str)
+{
+	// Corner Case
+	if (str.length === 0 || str[0] == 'N')
+		return null;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	let ip = new Array();
+    
+    let ip_str = str.split(" ");
+    
+	for (let i=0;i<ip_str.length;i++)
+		ip.push(ip_str[i]);
+
+	// Create the root of the tree
+    let root = new Node(parseInt(ip[0]));
+
+	// Push the root to the queue
+	let queue = new Array();
+	queue.push(root);
+
+	// Starting from the second element
+	let i = 1;
+	while (queue.length!==0 && i < ip.length) {
+
+		// Get and remove the front of the queue
+		let currNode = queue[0];
+		queue.shift();
+
+		// Get the current node's value from the string
+		let currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode.left = new Node(parseInt(currVal));
+
+			// Push it to the queue
+			queue.push(currNode.left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.length)
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode.right = new Node(parseInt(currVal));
+
+			// Push it to the queue
+			queue.push(currNode.right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    for(let i=0;i<t;i++)
+    {
+        let s_tree = readLine().trim();
+        let target = parseInt(readLine());
+        let k = parseInt(readLine());
+        let root = buildTree(s_tree);
+        let obj = new Solution();
+        let res = obj.KDistanceNodes(root,target,k);
+        let s = "";
+        for(let j = 0;j<res.length;j++){
+        	s = s+res[j]+" ";
+        }
+        console.log(s);        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/*
+class Node
+{
+    constructor(x){
+        this.key=x;
+        this.left=null;
+        this.right=null;
+    }
+}
+*/
+
+/**
+ * @param {Node} root
+ * @param {number} target
+ * @param {number} k
+ * @return {number[]}
+*/
+class Solution {
+  	KDistanceNodes(root,target,k){
+        //code here
+        
+        let v=[];
+        
+        const findKDistanceDownNode = (node, distance) => {
+            if (node == null || distance < 0) return;
+            
+            if (distance == 0) {
+                v.push(node.key);
+                return;
+            }
+            
+            findKDistanceDownNode(node.left, distance-1);
+            findKDistanceDownNode(node.right, distance-1);
+        }
+        
+        const findKDistanceNode = (node, target, distance) => {
+            if (node == null) return -1;
+            
+            if (node.key == target) {
+                findKDistanceDownNode(node, distance);
+                return 0;
+            }
+            
+            let dl = findKDistanceNode(node.left, target, distance);
+            
+            if (dl != -1) {
+                if (dl + 1 == distance) {
+                    v.push(node.key);
+                } else {
+                    findKDistanceDownNode(node.right, distance-dl-2);
+                }
+                return 1 + dl;
+            }
+            
+            let dr = findKDistanceNode(node.right, target, distance);
+            
+            if (dr != -1) {
+                if (dr + 1 == distance) {
+                    v.push(node.key);
+                } else {
+                    findKDistanceDownNode(node.left, distance-dr-2);
+                }
+                return 1 + dr;
+            }
+            
+            return -1;
+        }
+        
+        findKDistanceNode(root, target, k);
+        
+        v.sort((a, b) => a - b);
+        
+        return v;
+    }
+}
