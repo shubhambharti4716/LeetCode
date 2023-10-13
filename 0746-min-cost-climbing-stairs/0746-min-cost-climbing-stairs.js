9cd76c1593ef8d616ce8addf614451656f3e1cde
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-13 19:58:35 +0530

    Time: 64 ms (17.13%), Space: 42.2 MB (85.15%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -3,16 +3,7 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    if(cost.length ===1) return cost[0];
-
-    const minTotalCost = new Array(cost.length);
-
-    minTotalCost[0] = cost[0];
-    minTotalCost[1] = cost[1];
-
-    for(let i = 2; i < minTotalCost.length; i++) {
-        minTotalCost[i] = cost[i] + Math.min(minTotalCost[i - 1], minTotalCost[i-2]);
-    }
-
-    return Math.min(minTotalCost[cost.length-1], minTotalCost[cost.length-2])
+    for (let i = cost.length - 3; ~i; i--)
+        cost[i] += Math.min(cost[i+1], cost[i+2])
+    return Math.min(cost[0], cost[1])
 };
