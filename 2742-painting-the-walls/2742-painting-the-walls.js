928c8b98ffeb17f70af8796584c7d018937eae92
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-14 22:08:33 +0530

    Time: 94 ms (100.00%), Space: 47.3 MB (84.62%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} cost
+ * @param {number[]} time
+ * @return {number}
+ */
+var paintWalls = function(cost, time) {
+     const n = cost.length;
+        const dp = Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
+        dp[0] = 0;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = n; j >= 1; j--) {
+                dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
+            }
+        }
+        return dp[n];
+};
