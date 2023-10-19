f8979a2da4325b137f7708e9cb43b5573e3a92f1
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-19 19:42:05 +0530

    Time: 56 ms (43.70%), Space: 42.2 MB (73.02%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -4,31 +4,39 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    let index1 = s.length - 1
-    let index2 = t.length - 1
-    while (index1 >= 0 || index2 >= 0) {
-        let i1 = nextValidCharIdx(s, index1)
-        let i2 = nextValidCharIdx(t, index2)
-        if (i1 < 0 && i2 < 0) return true
-        if (i1 < 0 || i2 < 0) return false
-        if (s[i1] !== t[i2]) return false
-        index1 = i1 - 1
-        index2 = i2 - 1
+    var ss=[];
+    var ts=[];
+    var i=0;
+    while(i<s.length)
+    {
+      if(s[i]=="#")
+      {
+        ss.pop();
+      }
+      else
+      {
+        ss.push(s[i]);
+      }
+      i++;
     }
-    return true
+    var j=0;
+    while(j<t.length)
+    {
+      if(t[j]=="#")
+      {
+        ts.pop();
+      }
+      else
+      {
+        ts.push(t[j]);
+      }
+      j++;
+    }
+    var f=ss.join("");
+    var l=ts.join("");
+    if(f==l)
+    {
+      return true;
+    }
+    return false;
 };
-
-function nextValidCharIdx(str, index) {
-    let backspaceCount = 0
-    while (index >= 0) {
-        if (str[index] === '#') {
-            backspaceCount += 1
-        } else if (backspaceCount > 0) {
-            backspaceCount -= 1
-        } else {
-            break
-        }
-        index -= 1
-    }
-    return index
-}
