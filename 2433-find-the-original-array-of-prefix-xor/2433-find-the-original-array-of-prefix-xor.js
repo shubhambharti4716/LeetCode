fb1e9a23220a861523b7ea90c02828867ee58196
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-10-31 21:35:13 +0530

    Time: 163 ms (57.14%), Space: 70.9 MB (76.37%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -2,10 +2,12 @@
  * @param {number[]} pref
  * @return {number[]}
  */
-var findArray = function(pref) {
-        let result = [pref[0]];
-        for(let i = 1; i < pref.length; i++) {
-            result.push(pref[i] ^ pref[i-1]);
-        }
-        return result;
+var findArray = function (pref) {
+    let arr = [];
+    arr[0] = pref[0];
+
+    for (let i = 1; i < pref.length; i++) {
+        arr[i] = pref[i] ^ pref[i - 1];
     }
+    return arr;
+};
