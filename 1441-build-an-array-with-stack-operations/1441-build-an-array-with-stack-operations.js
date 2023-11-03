13c55a37f9357858604a72aceee63ba236f568d1
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-03 20:49:45 +0530

    Time: 57 ms (26.04%), Space: 42.1 MB (32.29%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.js
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.js
@@ -4,25 +4,14 @@
  * @return {string[]}
  */
 var buildArray = function(target, n) {
-    //using 2 pointer approach
-    const operations = [];
-    let targetIndex = 0; // Pointer for target array
-    let currentNumber = 1; // Pointer for numbers 1 to n
-
-    while (targetIndex < target.length) {
-        if (target[targetIndex] === currentNumber) {
-            // If the current number in the target array matches the current number 1 to n
-            // Push "Push" operation and move both pointers
-            operations.push("Push");
-            targetIndex++;
-        } else {
-            // If the numbers don't match, push "Push" followed by "Pop" operation
-            // and only move the current number pointer
-            operations.push("Push");
-            operations.push("Pop");
-        }
-        currentNumber++;
+    let res = [];
+    j = 0;
+    for(let i=1;i<=n;i++) {
+        if(target[j]===i) {
+            res.push('Push');
+            j++;
+        } else res.push(...['Push','Pop']);
+        if(j===target.length) return res;
     }
-
-    return operations;
+    return res;
 };
