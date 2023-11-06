6eebc62714a6af55ca688fd026421d9477cb1f50
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-07 00:08:59 +0530

    Time: 42 ms (69.06%), Space: 88.5 MB (37.70%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.java
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -1,28 +1,59 @@
 class SeatManager {
 
-    boolean[] isSeatReserved;
-    int nextSeat = 1;
-
-    TreeSet<Integer> unreservedSeats = new TreeSet<>();
-
+    int[] arr;
+    int low, size;
     public SeatManager(int n) {
-        isSeatReserved = new boolean[n+1];
+        arr = new int[n+1];
+
+        for(int i = 0; i<=n; i++) {
+            arr[i] = i;
+        }
+        low = 1;
+        size = n;
+    }
+
+    private void swap(int ind1, int ind2) {
+        int temp = arr[ind1];
+        arr[ind1] = arr[ind2];
+        arr[ind2] = temp;
+    }
+
+    private void heapify(int ind) {
+        int left = 2*ind;
+        int right = 2*ind + 1;
+        int index = -1;
+        if(right<=size) {
+            index = arr[left]<=arr[right]?left:right;
+        } else if(left<=size) {
+            index = left;
+        }
+        if(index!=-1 && arr[index]<arr[ind]) {
+            swap(ind, index);
+            heapify(index);
+        }
     }
 
     public int reserve() {
-        int seat;
-        if (unreservedSeats.isEmpty()) {
-            seat = nextSeat;
-            nextSeat++;
-        } else {
-            seat = unreservedSeats.first();
-            unreservedSeats.remove(seat);
-        }
-        return seat;
+        int ans = arr[low];
+        swap(low, size);
+        size --;
+        heapify(low);
+        return ans;
     }
-
+    
     public void unreserve(int seatNumber) {
-        isSeatReserved[seatNumber] = false;
-        unreservedSeats.add(seatNumber);
+        arr[++size] = seatNumber;
+        int index = size;
+        while(arr[index]<arr[index/2]) {
+            swap(index, index/2);
+            index = index/2;
+        }
     }
 }
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
