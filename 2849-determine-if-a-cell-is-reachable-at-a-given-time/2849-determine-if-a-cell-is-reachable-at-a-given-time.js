a256ddb2f0a2f9447a6941a7403e4664f2a30657
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-08 11:42:33 +0530

    Time: 54 ms (71.04%), Space: 43.8 MB (24.43%) - LeetHub

---
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.js
@@ -0,0 +1,32 @@
+var isReachableAtTime = function(startX, startY, targetX, targetY, time) {
+    // Check if the starting point is the same as the target point.
+    if (startX === targetX && startY === targetY) {
+        // If they are the same, check if time is 1, and return false (impossible to stay at the same cell for 1 second).
+        if (time === 1) {
+            return false;
+        }
+    }
+    
+    // Calculate the absolute differences in x and y coordinates between the starting and target points.
+    let xDifference = startX - targetX;
+    let yDifference = startY - targetY;
+    
+    // Ensure that xDifference and yDifference are non-negative.
+    if (xDifference < 0) {
+        xDifference = -xDifference;
+    }
+    if (yDifference < 0) {
+        yDifference = -yDifference;
+    }
+    
+    // Calculate the maximum difference between xDifference and yDifference.
+    let maxDifference = Math.max(xDifference, yDifference);
+    
+    // Check if the maximum difference is less than or equal to the given time.
+    if (maxDifference <= time) {
+        return true; // It's possible to reach the target within the specified time.
+    }
+    
+    // If the maximum difference is greater than the given time, return false.
+    return false;
+}
