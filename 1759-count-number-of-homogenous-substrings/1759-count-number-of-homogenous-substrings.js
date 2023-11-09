782fdb662d7b9d556d99b82776b607e0f7c6ba47
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-09 23:36:42 +0530

    Time: 53 ms (100.00%), Space: 46.2 MB (20.00%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.js
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.js
@@ -3,25 +3,16 @@
  * @return {number}
  */
 var countHomogenous = function(s) {
-    let res = 0;
-    let start = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== s[start]) {
-            let appear = i - start;
-            while (appear > 0) {
-                res += appear;
-                appear -= 1;
-            }
-            start = i;
-        }
-    }
-
-    let appear = s.length - start;
-    while (appear > 0) {
-        res += appear;
-        appear -= 1;
-    }
-
-    return res % (Math.pow(10, 9) + 7);
+    let i = 0,
+    res = 0,
+    cur = 0,
+    count = 0,
+    mod = 1_000_000_007;
+  while (i < s.length) {
+    count = s.charAt(i) === cur ? count + 1 : 1;
+    cur = s.charAt(i);
+    res = (res + count) % mod;
+    i++;
+  }
+  return res;
 };
