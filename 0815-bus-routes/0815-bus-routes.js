646c40f8c8beb8d24d21bbf2d7f84249923e53ce
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-12 21:40:31 +0530

    Time: 560 ms (74.36%), Space: 117.5 MB (32.31%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.js
+++ 0815-bus-routes/0815-bus-routes.js
@@ -4,63 +4,42 @@
  * @param {number} target
  * @return {number}
  */
-var numBusesToDestination = function(routes, source, target) {
-  // This map will hold an adjacency map of all the routes each stop can go to
-  let stopRoutes = new Map();
-  
-  // Initialize adjacency map - for each route
-  for (let route = 0; route < routes.length; route++) {
-    // Look at all of the route's stops
-    for (let stop of routes[route]) {
-      // If we haven't added this stop yet then create a new entry for it
-      if (!stopRoutes.has(stop)) stopRoutes.set(stop, new Set());
-      // Add the current route as a possible route this stop can go to
-      stopRoutes.get(stop).add(route);
-    }
-  }
-  
-  // This queue will contain all of the visited stops AND the number of buses it took to get to that stop
-  let stopsToVisit = new Queue();
-  // We start at the source and 0 buses to get there
-  stopsToVisit.enqueue([source, 0]);
-  
-  // This set will contain all of the stops we've already visited
-  let visitedStops = new Set();
-  visitedStops.add(source);
-  
-  // This set will contain all of the routes we've visited
-  let visitedRoutes = new Array(routes.length);
-  
-  // While there are more stops to visit
-  while(!stopsToVisit.isEmpty()) {
-    // Get the queued stop and number of buses it took to get there
-    let stop = stopsToVisit.front()[0], bus = stopsToVisit.front()[1];
-    stopsToVisit.dequeue();
-    
-    // If the stop is our target then we return the number of buses it took to get here
-    if (stop === target) return bus;
-    
-    // Loop over all the routes this stop can go to
-    for (let route of stopRoutes.get(stop)) {
-      // If we've already visited this route then don't do it again
-      if (visitedRoutes[route]) continue;
-      
-      // Loop over all of the stops in this route
-      for (let stop of routes[route]) {
-        // If we haven't visited this stop before
-        if (!visitedStops.has(stop)) {
-          // Add it to the stops we've visited
-          visitedStops.add(stop);
-          // Add it to the queue of stops we need to visit adding 1 more bus stop to get to it
-          stopsToVisit.enqueue([stop, bus + 1]);
-        }
+var numBusesToDestination = function (routes, src, dest) {
+  const stops = {};
+
+  for (let r = 0; r < routes.length; r++) {
+    for (const s of routes[r]) {
+      if (!stops[s]) {
+        stops[s] = [];
       }
-      
-      // Set the current route as visited
-      visitedRoutes[route] = true;
+      stops[s].push(r);
     }
   }
-  
-  // If we never got to our target then return -1
+
+  const queue = new Queue();
+  const visited = new Set();
+
+  queue.enqueue([src, 0]);
+
+  while (!queue.isEmpty()) {
+    const [stop, min] = queue.dequeue();
+
+    if (stop === dest) {
+      return min;
+    }
+
+    for (const route of stops[stop]) {
+      if (visited.has(route)) {
+        continue;
+      }
+
+      for (const s of routes[route]) {
+        queue.enqueue([s, min + 1]);
+      }
+
+      visited.add(route);
+    }
+  }
+
   return -1;
 };
