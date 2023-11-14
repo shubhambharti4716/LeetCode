15d1f449f27f99f94f7e18e1d6bea68ea6613c7a
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-14 23:12:23 +0530

    Time: 425 ms (87.91%), Space: 46.8 MB (87.91%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.js
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.js
@@ -3,17 +3,39 @@
  * @return {number}
  */
 var countPalindromicSubsequence = function(s) {
+    const visited = new Set();
     let res = 0;
-    const uniq = new Set(s);
 
-    for (const c of uniq) {
-        const start = s.indexOf(c);
-        const end = s.lastIndexOf(c);
+    for (let left = 0; left < s.length - 2; left++) {
+        // console.log("left", left);
+        const leftChar = s[left];
 
-        if (start < end) {
-            res += new Set(s.slice(start + 1, end)).size;
+        if (visited.has(leftChar)) continue;
+        visited.add(leftChar);
+
+        const seenMiddleLetters = new Set();
+        let rightMostMatchIdx;
+
+        for (let right = s.length - 1; right >= 0; right--) {
+            const rightChar = s[right];
+            if (rightChar === leftChar) {
+                rightMostMatchIdx = right;
+                break;
+            }
         }
+
+        if (rightMostMatchIdx == null || rightMostMatchIdx - left <= 1) continue;
+
+        console.log("right", rightMostMatchIdx);
+        for (let i = left + 1; i < rightMostMatchIdx; i++) {
+            seenMiddleLetters.add(s[i]);
+        }
+
+        // console.log("seenMiddleLetters", seenMiddleLetters);
+
+        res += seenMiddleLetters.size;
+        // console.log("updated res", res);
     }
 
-    return res;    
+    return res;
 };
