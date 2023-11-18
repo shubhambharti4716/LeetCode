297d1befb41109756e344dbcafb681ba93bd1143
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-18 18:11:06 +0530

    Time: 202 ms (43.51%), Space: 57.1 MB (16.79%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    let i = 0, j = 0, sum = 0, maxLength = 0;
+
+    for (i = 0; i < nums.length; i++) {
+        sum += nums[i];
+
+        while ((i - j + 1) * nums[i] - sum > k) {
+            sum -= nums[j];
+            j++;
+        }
+
+        maxLength = Math.max(maxLength, i - j + 1);
+    }
+
+    return maxLength;
+};
