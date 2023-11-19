7941db8d276ac955e6df14cdec6707db1628076a
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-19 15:16:25 +0530

    Time: 168 ms (100.00%), Space: 54.3 MB (60.87%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.js
@@ -2,18 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var reductionOperations = function(nums) {
-    const n = nums.length;
-    const freq = new Array(50001).fill(0);
-    for (let i = 0; i < n; i++) {
-        freq[nums[i]]++;
-    }
-    let res = 0, operations = 0;
-    for (let i = 50000; i >= 1; i--) {
-        if (freq[i] > 0) {
-            operations += freq[i];
-            res += operations - freq[i];
-        }
-    }
-    return res;
+var reductionOperations = function(nums) 
+{
+    nums.sort((a,b)=>a-b);
+    let count = 0;
+    for(let i = nums.length - 1;i>0;i--)
+        if(nums[i] !== nums[i-1])
+            count += nums.length - i
+    return count
 };
