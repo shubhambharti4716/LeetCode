78de8620e35eac5d6d312a63090665e9815f488f
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-27 08:53:05 +0530

    Time: 120 ms (82.50%), Space: 57 MB (62.50%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.js
+++ 0935-knight-dialer/0935-knight-dialer.js
@@ -2,52 +2,63 @@
  * @param {number} n
  * @return {number}
  */
-var knightDialer = function(n) {
 
-	// if n equals 1, 10 number can be formed, that is with each digit
-    if(n === 1) {
-        return 10;
+var getNext = function(pos){
+    switch (pos){
+        case 0:
+            return [4,6]
+        case 1:
+            return [8,6]
+        case 2:
+            return [7,9]
+                            case 3:
+            return [8,4]
+                            case 4:
+            return [0,3,9]
+                            case 5:
+            return []
+                            case 6:
+            return [0,1,7]
+                            case 7:
+            return [2,6]
+                            case 8:
+            return [1,3]
+                                                case 9:
+            return [2,4]
     }
-    const mod = 1000000007;
-	
-	// since knight can move in 8 directions, in the dialpad, we know what positions the knight can move when we start from a digit
-	// created the map of those positions that can be moved from key `num`
-    const map = {1: [6,8], 2: [7,9], 3: [4,8], 4: [0,3,9],
-                 5: [], 6: [0,1,7], 7: [2,6], 8: [1,3], 9: [2,4], 0: [4,6]};
+
+                    
     
-	// create a dp array of size - 10 * (n + 1), initialize with 0
-    const dp = Array(10).fill(null).map(() => Array(n + 1).fill(0));
-	
-	//initialize the dp array for each num and n = 1 and n = 2 
-    let total = 0;
-    for(let num = 0; num <= 9; ++num) {
-        dp[num][1] = 1;
-        dp[num][2] = map[num].length;
-        if(n === 2) {
-            total += dp[num][2];
+}
+
+var knightDialer = function(n) {
+    // dp[k][j] => knight at jth position in k jumps => distinct phone numbers
+    
+    let dp = Array(n+1).fill().map(()=>Array(10));
+    
+    for(let i=0;i<10;i++){
+        dp[1][i]=1
+    }
+    let op=0;
+    
+    if(n===1){
+        return 10
+    }
+    for(let i=2;i<=n;i++){
+        for(let j=0;j<10;j++){
+            dp[i][j]=0
+            let nextPoints = getNext(j);
+            for(let k=0;k<nextPoints.length;k++){
+                let nextP = nextPoints[k];
+                dp[i][j] = (dp[i][j] + dp[i-1][nextP])%1000000007
+            }
+            if(i===n){
+                op=(op+dp[i][j])%1000000007
+            }
         }
     }
-	
-	// if n > 2, we calculate the total numbers that can be formed from each digit when we start from it
-	// `total` will be summation of all the numbers which we will return
-    for(let i = 3; i <= n; ++i) {
-        for(let num = 0; num <= 9; ++num) {
-		
-			/* the number of digits that can be formed starting with `num` equals the sum of the number of digits that can be formed by the digits which are reachable from `num`
-			for instance, for n = 3 and num = 1, `16` and `18` are two 2 digit number. 
-			in order to get the 3 digit numbers starting with `1`, we need to check the digits reachable from 6 and 8
-			for `6` we check all the 2 digit numbers - 3 (0,1,7), and for `8` = 2 (1,3)
-			that is,
-			dp[1][3] = dp[6][2] + dp[8][2]
-			dp[6][2] = 3
-			dp[8][2] = 2
-			=> dp[1][3] = 5 (phone numbers - 160, 161, 167, 181, 183)
-			*/
-            map[num].forEach(d => dp[num][i] = (dp[num][i] + dp[d][i - 1]) % mod);
-            if(i === n) {
-                total = (total + dp[num][i]) % mod;
-            }
-        }    
-    }
-    return total;
+    
+    return op
+    
+    
 };
