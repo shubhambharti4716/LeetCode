c3a960ca08500565f5bf2d26ece486ca57f6ee6c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-30 22:40:00 +0530

    Time: 53 ms (44.44%), Space: 41.5 MB (100.00%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var minimumOneBitOperations = function(n) {
+    if (n <= 1) return n;
+    let count = 0;
+    while ((1 << count) <= n) count++;
+    return ((1 << count) - 1) - minimumOneBitOperations(n - (1 << (count - 1)));
+};
