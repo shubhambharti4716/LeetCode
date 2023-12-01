4805ee46a7070998fbfffd5818c69ae48ef90224
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-01 20:08:34 +0530

    Time: 53 ms (43.51%), Space: 42.1 MB (52.70%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -3,8 +3,8 @@
  * @param {string[]} word2
  * @return {boolean}
  */
-var arrayStringsAreEqual = function(word1, word2) {
-    const str1 = word1.join('');
-    const str2 = word2.join('');
-    return str1 === str2;
+var arrayStringsAreEqual = function (word1, word2) {
+    let s1 = word1.join("")
+    let s2 = word2.join("")
+    return s1===s2
 };
