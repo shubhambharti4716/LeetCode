0fa68bfbfb18d6d709e59eaed0b9a4401af1de48
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-02 11:40:30 +0530

    Time: 92 ms (89.22%), Space: 50 MB (70.26%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
@@ -4,37 +4,24 @@
  * @return {number}
  */
 var countCharacters = function(words, chars) {
-    const counts = new Array(26).fill(0);
+    let sum = 0
+    for(let i = 0; i < words.length; i++) {
+        let count = 0
+        let arr = JSON.stringify(chars)
+        for(let j = 0; j < words[i].length; j++) {
+            let a = words[i][j]
 
-    // Step 1: Initialize Character Counts Array
-    for (const ch of chars) {
-        counts[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
-    }
-
-    let result = 0;
-
-    // Step 3: Check Words
-    for (const word of words) {
-        if (canForm(word, counts)) {
-            // Step 4: Calculate Lengths
-            result += word.length;
-        }
-    }
-
-    return result;
-
-    function canForm(word, counts) {
-        const c = new Array(26).fill(0);
-
-        // Step 2: Update Counts Array
-        for (const ch of word) {
-            const x = ch.charCodeAt(0) - 'a'.charCodeAt(0);
-            c[x]++;
-            if (c[x] > counts[x]) {
-                return false;
+            if (arr.includes(a)) {
+                count++
+                //let index = arr.indexOf(a);
+                arr = arr.replace(a, "");
+            } else {
+                break;
             }
         }
-
-        return true;
+        if (count === words[i].length) {
+            sum = sum + count
+        }
     }
+    return sum
 };
