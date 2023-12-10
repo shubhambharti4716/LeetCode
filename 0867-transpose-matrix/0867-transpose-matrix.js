b4c538af329aa5e3df7110bb3307e65ba612200b
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-10 10:20:19 +0530

    Time: 58 ms (79.95%), Space: 44.6 MB (48.93%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.js
+++ 0867-transpose-matrix/0867-transpose-matrix.js
@@ -3,15 +3,18 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-    let row = matrix.length;
-    let col = matrix[0].length;
-    let result = Array.from({ length: col }, () => Array(row).fill(0));
+
+    let row = matrix.length
+    let col = matrix[0].length
     
-    for (let i = 0; i < col; ++i) {
-        for (let j = 0; j < row; ++j) {
-            result[i][j] = matrix[j][i];
-        }
-    }
-    
-    return result;
+   let arr = Array(col).fill(0).map(x => Array(row).fill(0))
+
+
+   for(let i=0;i<row;i++){
+       for(let j=0;j<col;j++){
+           arr[j][i] = matrix[i][j]
+       }
+   }
+
+  return arr
 };
