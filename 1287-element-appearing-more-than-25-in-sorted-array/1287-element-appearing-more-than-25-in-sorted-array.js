c04fdd5b6895fcda51b3b2294c105cafa453ea28
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-11 10:19:52 +0530

    Time: 52 ms (79.26%), Space: 42.9 MB (60.64%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function(arr) {
+    let size = arr.length;
+    let qtr = Math.floor(size / 4);
+    let cnt = 1;
+    let p = arr[0];
+
+    for (let i = 1; i < size; i++) {
+        if (p === arr[i]) {
+            cnt++;
+        } else {
+            cnt = 1;
+        }
+
+        if (cnt > qtr) {
+            return arr[i];
+        }
+
+        p = arr[i];
+    }
+
+    return p;
+
+};
