c0ccd31c31c4ea453dc85f25c1080ff0e66e3457
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-12 19:03:51 +0530

    Time: 61 ms (26.88%), Space: 42.8 MB (47.18%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,17 +3,6 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max1 = Number.MIN_SAFE_INTEGER;
-    let max2 = Number.MIN_SAFE_INTEGER;
-
-    for (const num of nums) {
-        if (num >= max1) {
-            max2 = max1;
-            max1 = num;
-        } else if (num > max2) {
-            max2 = num;
-        }
-    }
-
-    return (max1 - 1) * (max2 - 1);
+    let temp = nums.sort((a,b) => b - a)
+    return (temp[0]-1) * (temp[1]-1)
 };
