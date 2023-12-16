e14963794a67ec5742d63d5a3fe5bb6687f50cfa
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-16 10:14:14 +0530

    Time: 55 ms (96.36%), Space: 42.5 MB (87.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,14 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-
-        const charMap = Array(26).fill(0);
-
-        for (let i = 0; i < s.length; i++) {
-            charMap[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-            charMap[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    var freq = new Array(26).fill(0);
+    for (var i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        freq[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+    
+    for (var i = 0; i < freq.length; i++) {
+        if (freq[i] !== 0) {
+            return false;
         }
-
-        return charMap.every(count => count === 0);
+    }
+    
+    return true;
 };
