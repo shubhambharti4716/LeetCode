7beb3052ef7d41c2e4761c80f9e46cb7514c6eb7
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-20 16:50:51 +0530

    Time: 88 ms (50.00%), Space: 47.1 MB (23.70%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.js
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -4,11 +4,20 @@
  * @return {number}
  */
 var buyChoco = function(prices, money) {
-    let sortedPrices = prices.sort((a,b) => {return a-b});
-    for(let i = 0; i < sortedPrices.length; i++){
-        if(prices[i] + prices[i+1] > money){
-            return money;
-        }
-        return money - (prices[i] + prices[i+1]);
+    prices.sort(function(a , b) {
+       return a - b
+    })
+    let cost = prices[0] + prices[1]
+    return cost > money ? money : money - cost ?? 0
+
+    if(money == cost)
+        return 0
+    if(cost > money) {
+        return money
     }
+
+    if(cost < money ) {
+        return money - cost
+    }
+
 };
