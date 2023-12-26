a0264bd253761876b74d1393514cc165b03c4abd
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-26 17:40:24 +0530

    Time: 74 ms (67.54%), Space: 43.8 MB (79.82%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
@@ -1,28 +1,23 @@
-/**
- * @param {number} n
- * @param {number} k
- * @param {number} target
- * @return {number}
- */
-const mod = 1e9 + 7;
-const numRollsToTarget = (d, f, target) => {
-    let dp = initialize2DArrayNew(d + 1, target + 1);
-    dp[0][0] = 1;
-    for (let i = 1; i <= d; i++) {
-        for (let j = 1; j <= f; j++) {
-            for (let k = j; k <= target; k++) {
-                dp[i][k] = (dp[i][k] + dp[i - 1][k - j]) % mod;
-            }
-        }
-    }
-    return dp[d][target];
-};
+// Time complexity: O(n * k * target)
+// Space complexity: O(n * target)
 
-const initialize2DArrayNew = (m, n) => {
-    let data = [];
-    for (let i = 0; i < m; i++) {
-        let tmp = new Array(n).fill(0);
-        data.push(tmp);
-    }
-    return data;
+const MOD = 1e9 +7;
+var numRollsToTarget = function(n, k, target) {
+	const memo = Array(n + 1).fill(-1).map(x => Array(target + 1).fill(-1));
+	return helper(n, k, target, memo);
+};
+		
+var helper = function(n, k, target, memo) {
+	if (n === 0 || target < 0)
+		return target === 0 ? 1 : 0;
+	
+	if (memo[n][target] !== -1)
+			return memo[n][target];
+
+	let ways = 0;
+
+	for (let i = 1; i <= k; i++)
+		ways = (ways + helper(n - 1, k, target - i, memo)) % MOD;
+
+	return memo[n][target] = ways;
 };
