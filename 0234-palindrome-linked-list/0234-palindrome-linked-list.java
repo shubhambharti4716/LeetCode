f9c37c3a0802b323f7e45f0f193ba47d2e1d1086
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-22 14:05:34 +0530

    Time: 3 ms (99.85%), Space: 68.6 MB (30.80%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,51 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode findMiddle(ListNode head) {
-        
-        ListNode hare = head;
-        ListNode turtle = head;
-        while(hare.next != null && hare.next.next != null) {
-            hare = hare.next.next;
-            turtle = turtle.next;
-        }
-
-        return turtle;
-    }
-
-    public ListNode reverse(ListNode head) {
-
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while(curr != null) {
-            ListNode nex = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nex;
-        }
-
-        return prev;
-    }
-
     public boolean isPalindrome(ListNode head) {
+        ListNode start = head;
+        ListNode mid = head;
+        ListNode fast = head;
         
-        if(head == null || head.next == null) {
-            return true;
+        while(fast != null && fast.next != null){
+            mid = mid.next;
+            fast= fast.next.next;
         }
+        
+        ListNode prev= null;
+        ListNode next = null;
+        
+        while(mid != null){
+            next = mid.next;
+            mid.next = prev;
+            prev= mid;
+            mid = next;
+        }
+        
+        while(prev!= null){
 
-        ListNode mid = findMiddle(head);
-        ListNode secondHead = reverse(mid.next);
-
-        ListNode firstHead = head;
-        while(secondHead != null) {
-            if(firstHead.val != secondHead.val)
+            if(prev.val!= start.val){
                 return false;
-            
-            firstHead = firstHead.next;
-            secondHead = secondHead.next;
+            }
+            prev= prev.next;
+            start= start.next;
         }
-
         return true;
     }
 }
