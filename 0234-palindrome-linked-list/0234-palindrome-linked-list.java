d5c818ac97a7adefd25f6acc040ddf347f9b30dd
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-22 14:05:11 +0530

    Time: 6 ms (49.88%), Space: 68.7 MB (30.80%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,19 +9,51 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new ArrayList();
-        while(head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
+    public ListNode findMiddle(ListNode head) {
         
-        int left = 0;
-        int right = list.size()-1;
-        while(left < right && list.get(left) == list.get(right)) {
-            left++;
-            right--;
+        ListNode hare = head;
+        ListNode turtle = head;
+        while(hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
         }
-        return left >= right;
+
+        return turtle;
+    }
+
+    public ListNode reverse(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode nex = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+        }
+
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        
+        if(head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode mid = findMiddle(head);
+        ListNode secondHead = reverse(mid.next);
+
+        ListNode firstHead = head;
+        while(secondHead != null) {
+            if(firstHead.val != secondHead.val)
+                return false;
+            
+            firstHead = firstHead.next;
+            secondHead = secondHead.next;
+        }
+
+        return true;
     }
 }
