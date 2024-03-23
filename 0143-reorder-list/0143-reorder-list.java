a5e034b1d9922499b91ffa07dd8d650e417a199e
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-23 15:41:45 +0530

    Time: 3 ms (23.73%), Space: 48.4 MB (22.58%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,42 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head) {
-        if (head == null) return null;
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode nextNode = null;
-        while (curr != null) {
-            nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        return prev;
-    }
-
-    public void merge(ListNode list1, ListNode list2) {
-        while (list2 != null) {
-            ListNode nextNode = list1.next;
-            list1.next = list2;
-            list1 = list2;
-            list2 = nextNode;
-        }
-    }
-
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) return;
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = head;
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            fast = fast.next.next;
-            slow = slow.next;
+        
+        ListNode cur = head;
+        
+        List<ListNode> list = new ArrayList<>();  
+        while (cur != null){
+            list.add(cur);
+            cur = cur.next;
         }
-        prev.next = null;
-        ListNode list1 = head;
-        ListNode list2 = reverse(slow);
-        merge(list1, list2);
+        cur = head;  
+        int l = 1, r = list.size() - 1;  
+        int count = 0;
+        while (l <= r){
+            if (count % 2 == 0){
+                
+                cur.next = list.get(r);
+                r--;
+            }else {
+                
+                cur.next = list.get(l);
+                l++;
+            }
+            
+            cur = cur.next;
+            count++;
+        }
+        
+        cur.next = null;
     }
 }
