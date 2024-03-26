806ae0b833780f743c30d9af543934fbfae41b1e
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-26 09:21:42 +0530

    Time: 1 ms (98.00%), Space: 55.1 MB (43.10%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,47 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-             int n = nums.length;
-        boolean contains1 = false;
+        int n = nums.length;
+        boolean[] found = new boolean[n+1];
+        for(int i = 0;i< n; i++){
+            if (nums[i]>0 && nums[i]<= n){
+                found[nums[i]]=true;
 
-        // Replace negative numbers, zeros,
-        // and numbers larger than n with 1s.
-        // After this nums contains only positive numbers.
-        for (int i = 0; i < n; i++) {
-            // Check whether 1 is in the original array
-            if (nums[i] == 1) {
-                contains1 = true;
             }
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = 1;
-            }      
         }
-
-        if (!contains1) return 1;
-
-        // Mark whether integers 1 to n are in nums
-        // Use index as a hash key and negative sign as a presence detector.
-        for (int i = 0; i < n; i++) {
-            int value = Math.abs(nums[i]);
-            if (value == n) {
-                nums[0] = - Math.abs(nums[0]);
-            } else {
-                nums[value] = - Math.abs(nums[value]);
-            }       
-        }
-
-        // First positive in nums is smallest missing positive integer
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > 0)
+        for(int i= 1;i<=n;i++){
+            if(!found[i]){
                 return i;
+            }
         }
-
-        // nums[0] stores whether n is in nums
-        if (nums[0] > 0){
-            return n;
-        }
-            
-        // If nums contains all elements 1 to n
-        // the smallest missing positive number is n + 1
-        return n + 1;
+        return n+1;
     }
 }
