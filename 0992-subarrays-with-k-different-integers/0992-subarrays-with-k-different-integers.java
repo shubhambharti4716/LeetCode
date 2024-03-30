589bc9e8c5d731d69df900d5092ecc7d9e3e855c
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-30 10:34:25 +0530

    Time: 4 ms (99.30%), Space: 46.3 MB (79.58%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,28 +1,34 @@
 class Solution {
-    public int calcsubarray(int[] nums, int k) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        int i=0;
-        int j=0;
-        int ans=0;
-        int n=nums.length;
-        while(j<n){
-            hs.put(nums[j],hs.getOrDefault(nums[j],0)+1);
-            if (hs.size() > k) {
-                while (hs.size() > k) {
-                    hs.put(nums[i], hs.get(nums[i]) - 1);
-                    if (hs.get(nums[i]) == 0)
-                        hs.remove(nums[i]);
-                    i++;
-                }
-            }
-            ans += j - i + 1;
-            j++;
-        }
-        return ans;
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k - 1);
     }
 
+    private int helper(int nums[], int k) {
+        if (k == 0) {
+            return 0;
+        } else {
+            int count[] = new int[nums.length + 1];
+            int windowStart = 0, windowEnd = 0, res = 0;
+            int currCount = 0;
 
-    public int subarraysWithKDistinct(int[] nums, int k){
-        return calcsubarray(nums,k)-calcsubarray(nums,k-1);
+            while (windowEnd != nums.length) {
+                int curr = nums[windowEnd++];
+                
+                if (count[curr] == 0) {
+                    currCount++;
+                }
+                count[curr]++;
+
+                while (currCount > k) {
+                    if (--count[nums[windowStart++]] == 0) {
+                        currCount--;
+                    }
+                }
+
+                res += windowEnd - windowStart;
+            }
+
+            return res;
+        }
     }
 }
