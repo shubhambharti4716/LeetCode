aae730ff6199671436c59232be2c88e32969c1b4
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-30 10:34:59 +0530

    Time: 3 ms (100.00%), Space: 46.3 MB (81.93%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,34 +1,20 @@
 class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1);
-    }
-
-    private int helper(int nums[], int k) {
-        if (k == 0) {
-            return 0;
-        } else {
-            int count[] = new int[nums.length + 1];
-            int windowStart = 0, windowEnd = 0, res = 0;
-            int currCount = 0;
-
-            while (windowEnd != nums.length) {
-                int curr = nums[windowEnd++];
-                
-                if (count[curr] == 0) {
-                    currCount++;
-                }
-                count[curr]++;
-
-                while (currCount > k) {
-                    if (--count[nums[windowStart++]] == 0) {
-                        currCount--;
-                    }
-                }
-
-                res += windowEnd - windowStart;
+     public int subarraysWithKDistinct(int[] nums, int K) {
+        int[] numFreq = new int[nums.length+1];
+        int distinct = 0, start = 0, minEnd = 0, total = 0;
+        while (distinct == K || minEnd < nums.length) {
+            while (distinct < K && minEnd < nums.length) 
+			    if (numFreq[nums[minEnd++]]++ == 0) 
+                distinct++;
+            int maxEnd = minEnd;
+            while (maxEnd < nums.length && numFreq[nums[maxEnd]] > 0)
+			    maxEnd++;
+            while (distinct == K) {
+                if (numFreq[nums[start++]]-- == 1)
+                distinct--;
+                total += (maxEnd - minEnd + 1);
             }
-
-            return res;
         }
+        return total;
     }
-}
+ }
