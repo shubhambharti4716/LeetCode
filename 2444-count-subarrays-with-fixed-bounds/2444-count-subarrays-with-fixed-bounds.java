058d653f6ec2935faeaae8ca6e438c471efd46ce
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-03-31 19:29:39 +0530

    Time: 8 ms (51.62%), Space: 59.4 MB (83.03%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,27 +1,26 @@
-class Solution {
-    public long countSubarrays(int[] nums, int minK, int maxK) {
-        int n = nums.length;
-        long result = 0;
-        int minKIndex = -1;
-        int maxKIndex = -1;
-        int culpritIndex = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minK || nums[i] > maxK) {
-                culpritIndex = i;
+class Solution 
+{
+    public long countSubarrays(int[] a, int m, int M) 
+    {
+        int x=-1;
+        int y=-1;
+        int l=0;
+        long c=0;
+        for(int i=0; i<a.length; i++)
+        {
+            if(a[i]<m || a[i]>M)
+            {
+                l = i+1;
+                x = i;
+                y = i;
+                continue;
             }
-
-            if (nums[i] == minK)
-                minKIndex = i;
-
-            if (nums[i] == maxK)
-                maxKIndex = i;
-
-            long smaller = Math.min(minKIndex, maxKIndex);
-            long temp = smaller - culpritIndex;
-
-            result += temp <= 0 ? 0 : temp;
+            if(a[i] == m)
+                x = i;
+            if(a[i] == M)
+                y = i;
+            c+= (i-l+1) - (i - (Math.min(x,y)+1)+1);
         }
-        return result;
+        return c;
     }
 }
