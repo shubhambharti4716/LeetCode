52ae01eddcca791c889ecdfb7f7cdae0cf73de89
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-07 21:52:50 +0530

    Time: 61 ms (16.97%), Space: 49.2 MB (23.83%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+    let leftMin = 0, leftMax = 0;
+
+    for (let c of s) {
+        if (c === '(') {
+            leftMin++;
+            leftMax++;
+        } else if (c === ')') {
+            leftMin--;
+            leftMax--;
+        } else {
+            leftMin--;
+            leftMax++;
+        }
+        if (leftMax < 0) return false;
+        if (leftMin < 0) leftMin = 0;
+    }
+    
+    return leftMin === 0;
+};
