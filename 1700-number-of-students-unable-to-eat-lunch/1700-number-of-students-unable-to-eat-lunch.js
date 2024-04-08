94015ae970da4d99870299ff76424e1c9096e4ba
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-08 06:16:08 +0530

    Time: 47 ms (86.33%), Space: 48.5 MB (88.86%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -3,85 +3,35 @@
  * @param {number[]} sandwiches
  * @return {number}
  */
- class QueueNode {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class Queues {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    enqueue(data) {
-        let newNode = new QueueNode(data);
-
-        if (this.tail === null) {
-            this.head = this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        
-        this.size++;
-    }
-
-    dequeue() {
-        if (this.isEmpty()) {
-            return null;
-        }
-
-        let removedNode = this.head;
-        this.head = this.head.next;
-
-        if (this.head === null) {
-            this.tail = null;
-        }
-
-        this.size--;
-        return removedNode.data;
-    }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    peek() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        return this.head.data;
-    }
-
-    getSize() {
-        return this.size;
-    }
-}
 var countStudents = function(students, sandwiches) {
-    const len = students.length
-    const studentsQueue = new Queues()
-    const foodStack = new Array()
+    let studentsList = students, sandwichesList = sandwiches;
 
-    for (let i = 0 ; i < len ; i++){
-        studentsQueue.enqueue(students[i])
-        foodStack.push(sandwiches[len - 1 - i])
-    }
-
-    let counter = 0
-    while(counter<foodStack.length){
-        if(studentsQueue.peek() == foodStack[foodStack.length - 1]){
-            studentsQueue.dequeue()
-            foodStack.pop()
-            counter = 0
-        }else{
-            const deqVal = studentsQueue.dequeue()
-            studentsQueue.enqueue(deqVal)
-            counter++
+    while (sandwichesList) {
+        let iterations = 0;
+        while (studentsList) {
+            if (sandwichesList[0] === studentsList[0]) {
+                sandwichesList.shift();
+                studentsList.shift();
+                // console.log('break');
+                // console.log(studentsList);
+                // console.log(sandwichesList);
+                iterations = 0;
+                if (sandwichesList.length === 0 || studentsList.length === 0) return studentsList.length;
+                break;
+            } else {
+                let shiftedStudent = studentsList.shift();
+                studentsList.push(shiftedStudent);
+                iterations++;
+                // console.log(`'iteration++ = ${iterations}`);
+                // console.log(studentsList);
+                // console.log(sandwichesList);
+            }
+            if (iterations === studentsList.length) {
+                // console.log('iteration = list length');
+                // console.log(studentsList);
+                // console.log(sandwichesList);
+                return studentsList.length
+            };
         }
     }
-    return counter
 };
