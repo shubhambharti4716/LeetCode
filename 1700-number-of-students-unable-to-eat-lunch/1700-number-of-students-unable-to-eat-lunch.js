58d03d2330aafe1cd6eda0eeed4794a807e3d4a3
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-08 06:16:41 +0530

    Time: 44 ms (94.68%), Space: 48.5 MB (84.30%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -4,34 +4,13 @@
  * @return {number}
  */
 var countStudents = function(students, sandwiches) {
-    let studentsList = students, sandwichesList = sandwiches;
-
-    while (sandwichesList) {
-        let iterations = 0;
-        while (studentsList) {
-            if (sandwichesList[0] === studentsList[0]) {
-                sandwichesList.shift();
-                studentsList.shift();
-                // console.log('break');
-                // console.log(studentsList);
-                // console.log(sandwichesList);
-                iterations = 0;
-                if (sandwichesList.length === 0 || studentsList.length === 0) return studentsList.length;
-                break;
-            } else {
-                let shiftedStudent = studentsList.shift();
-                studentsList.push(shiftedStudent);
-                iterations++;
-                // console.log(`'iteration++ = ${iterations}`);
-                // console.log(studentsList);
-                // console.log(sandwichesList);
-            }
-            if (iterations === studentsList.length) {
-                // console.log('iteration = list length');
-                // console.log(studentsList);
-                // console.log(sandwichesList);
-                return studentsList.length
-            };
+    while (students.length > 0 && students.indexOf(sandwiches[0]) !== -1) {
+        if (students[0] === sandwiches[0]) {
+            students.shift();
+            sandwiches.shift();
+            
         }
+        else students.push(students.shift());
     }
+    return students.length;
 };
