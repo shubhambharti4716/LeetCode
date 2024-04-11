520eccd2e11805e6674fbd7cdf72ebf6f8e072d5
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-11 23:22:35 +0530

    Time: 73 ms (25.41%), Space: 55.1 MB (72.61%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.js
+++ 0402-remove-k-digits/0402-remove-k-digits.js
@@ -3,23 +3,38 @@
  * @param {number} k
  * @return {string}
  */
-var removeKdigits = function(num, k) {
-    if(num.length ==k) return '0'
-    const res = []
-   for(let i=0;i<num.length;i++){
-        while(k>0 && res.length>0 && res[res.length-1]>num[i]){
-            res.pop()
-            k--
+const removeKdigits = (number, k) => {
+    // Stack to store digits of the resulting number
+    const digitStack = [];
+    // Variable to keep track of how many digits have been removed
+    let removed = 0;
+
+    // Iterate over each digit in the input number
+    for (const digit of number) {
+        // Remove digits from the stack if they are greater than the current digit and
+        // there are still digits to be removed
+        while (digitStack.length && digit < digitStack[digitStack.length - 1] && removed < k) {
+            digitStack.pop();
+            removed++;
         }
-        
-        if(res.length ==0 && num[i]=='0') continue
-        res.push(num[i])
-   }
+        // Push the current digit onto the stack
+        digitStack.push(digit);
+    }
 
-   while(k>0 && res.length>0) {
-    k--;
-    res.pop();
-   }
+    // Remove remaining digits if necessary
+    while (removed < k) {
+        digitStack.pop();
+        removed++;
+    }
 
-   return res.length ==0 ?'0':res.join('')
-};
+    // Remove leading zeros from the resulting number
+    while (digitStack.length && digitStack[0] === '0') {
+        digitStack.shift();
+    }
+
+    // Construct the result string by joining the digits in the stack
+    const result = digitStack.join('');
+
+    // Return "0" if the result is empty, otherwise return the result
+    return result ? result : "0";
+}
