22c64cf86542cd2fe88f10c2476f5cc558214e0d
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-04-12 19:30:13 +0530

    Time: 55 ms (77.60%), Space: 51.1 MB (55.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -1,23 +1,27 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
 var trap = function(height) {
-        let i = 0;
-        let left_max = height[0];
-        let sum = 0;
-        let j = height.length - 1;
-        let right_max = height[j];
-        while (i < j) {
-            if (left_max <= right_max) {
-                sum += left_max - height[i];
-                i++;
-                left_max = Math.max(left_max, height[i]);
+    let water = 0;
+    let left = 0;
+    let right = height.length - 1;
+    let maxLeft = 0;
+    let maxRight = 0;
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            if (height[left] >= maxLeft) {
+                maxLeft = height[left];
             } else {
-                sum += right_max - height[j];
-                j--;
-                right_max = Math.max(right_max, height[j]);
+                water += maxLeft - height[left];
             }
+            left++;
+        } else {
+            if (height[right] >= maxRight) {
+                maxRight = height[right];
+            } else {
+                water += maxRight - height[right];
+            }
+            right--;
         }
-        return sum;
     }
+
+    return water;
+};
